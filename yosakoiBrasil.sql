6aab7df341b3ff7d6682d6d5d478fa57221d4327
BRyuTakahashi <bruno.takahashi@sptech.school>
2022-11-20 17:01:54 -0300

    configurando site para a API

--- yosakoiBrasil.sql
+++ yosakoiBrasil.sql
@@ -4,9 +4,8 @@ USE yosakoiBrasil;
 CREATE TABLE usuario(
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(45),
-dtNasc DATE,
-email VARCHAR(45) CONSTRAINT chkEmail CHECK (email LIKE ('%@%')),
 apelido VARCHAR(45),
+email VARCHAR(45) CONSTRAINT chkEmail CHECK (email LIKE ('%@%')),
 senha VARCHAR(45)
 );
 
